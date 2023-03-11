e6b63f38e31a9ad2a28f70ea5e857838eaeb8e80
César Mayta <cesarmayta@gmail.com>
2023-03-10 22:05:22 -0500

    get

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Table,Container,Button,Form} from 'react-bootstrap'
 
 class App extends React.Component{
 
   constructor(props){
     super(props);
+    this.state = ({
+      tareas:[],
+      descripcion:'',
+      estado:'pendiente'
+    })
   }
 
   componentDidMount(){
@@ -12,13 +19,41 @@ class App extends React.Component{
     axios.get('http://localhost:5000/tarea')
     .then(res=>{
       console.log(res.data);
+      this.setState({
+        tareas : res.data.content
+      })
     })
   }
 
   render(){
     return(
       <div>
-        <h1>Lista de Tareas</h1>
+        <Container>
+          <h1>Lista de Tareas</h1>
+          <Table striped bordered hover variant="dark">
+            <thead>
+              <tr>
+                <th>Id</th>
+                <th>Tarea</th>
+                <th>Estado</th>
+                <th>Acciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.tareas.map((tarea)=>{
+                return(
+                  <tr key={tarea.id}>
+                    <td>{tarea.id}</td>
+                    <td>{tarea.descripcion}</td>
+                    <td>{tarea.estado}</td>
+                    <td>...</td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </Table>
+        </Container>
+        
       </div>
     )
   }
