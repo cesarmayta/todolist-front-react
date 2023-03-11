a2bce961a9891d192b087fc5659db880621da201
César Mayta <cesarmayta@gmail.com>
2023-03-10 22:57:07 -0500

    put

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,15 @@ class App extends React.Component{
     this.state = ({
       tareas:[],
       descripcion:'',
-      estado:'pendiente'
+      estado:'pendiente',
+      id:0,
+      pos:null,
+      tituloBoton:'Agregar Tarea'
     })
     this.cambioDescripcion = this.cambioDescripcion.bind(this);
     this.guardar = this.guardar.bind(this);
     this.eliminar = this.eliminar.bind(this);
+    this.mostrar = this.mostrar.bind(this);
   }
 
   cambioDescripcion(e){
@@ -24,24 +28,62 @@ class App extends React.Component{
     })
   }
 
+  mostrar(cod,index){
+    axios.get('http://localhost:5000/tarea/'+cod)
+    .then(res=>{
+      this.setState({
+        pos:index,
+        descripcion:res.data.content.descripcion,
+        id:res.data.content.id,
+        tituloBoton:'Actualizar Tarea'
+      })
+    })
+  }
+
   guardar(e){
     e.preventDefault();
+
+    let cod = this.state.id;
+
     const dataTarea = {
       descripcion : this.state.descripcion,
       estado : this.state.estado
     }
-    axios.post('http://localhost:5000/tarea',dataTarea)
-    .then(res=>{
-      console.log(res.data.content)
-      this.state.tareas.push(res.data.content)
-      var temp = this.state.tareas
-      this.setState({
-        descripcion:'',
-        tareas:temp
-      }).catch((error)=>{
-        alert(error.toString());
+    if(cod>0){
+      //actualizar
+      axios.put('http://localhost:5000/tarea/'+cod,dataTarea)
+      .then(res=>{
+        let indx = this.state.pos;
+        this.state.tareas[indx] = res.data.content;
+        var temp = this.state.tareas;
+        this.setState({
+          descripcion:'',
+          tareas:temp,
+          tituloBoton:'Agregar Tarea',
+          pos:null,
+          id:0
+        })
       })
-    })
+    }
+    else{
+      //insertar
+        axios.post('http://localhost:5000/tarea',dataTarea)
+      .then(res=>{
+        console.log(res.data.content)
+        this.state.tareas.push(res.data.content)
+        var temp = this.state.tareas
+        this.setState({
+          descripcion:'',
+          tareas:temp,
+          tituloBoton:'Agregar Tarea',
+          pos:null,
+          id:0
+        }).catch((error)=>{
+          alert(error.toString());
+        })
+      })
+    }
+    
   }
 
   eliminar(cod){
@@ -75,7 +117,7 @@ class App extends React.Component{
               <Form.Control type="text" value={this.state.descripcion} onChange={this.cambioDescripcion} />
             </Form.Group>
             <Button variant="primary" type="submit">
-              Agregar Tarea
+              {this.state.tituloBoton}
             </Button>
           </Form>
           <Table striped bordered hover variant="dark">
@@ -88,13 +130,16 @@ class App extends React.Component{
               </tr>
             </thead>
             <tbody>
-              {this.state.tareas.map((tarea)=>{
+              {this.state.tareas.map((tarea,index)=>{
                 return(
                   <tr key={tarea.id}>
                     <td>{tarea.id}</td>
                     <td>{tarea.descripcion}</td>
                     <td>{tarea.estado}</td>
-                    <td><Button variant="danger" onClick={()=>this.eliminar(tarea.id)}>Eliminar</Button></td>
+                    <td>                      
+                      <Button variant="success" onClick={()=>this.mostrar(tarea.id,index)}>Editar</Button>
+                      <Button variant="danger" onClick={()=>this.eliminar(tarea.id)}>Eliminar</Button>
+                    </td>
                   </tr>
                 )
               })}
